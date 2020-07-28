1b4aeb8d79c8651ef5ee9657004945ff0e4f4db9
Kathleen Kelly <murphy.kaite@gmail.com>
2020-07-28 15:24:10 -0700

    added schema

--- models/exercise.js
+++ models/exercise.js
@@ -3,11 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-
+    day: {
+        type: Date,
+        default: Date.now
     },
+    exercises: [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter workout type: resistance or cardio"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Exercise"
+            },
+            duration: {
+                type: Number,
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+
+        }]
 });
 
-const Exercise = mongoose.model("Exercise", exerciseSchema);
+const Workout = mongoose.model("Workout", exerciseSchema);
 
-module.exports = Exercise;
+module.exports = Workout;
